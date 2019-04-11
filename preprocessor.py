c2a9ccf781593389b6b7efdfc54fd9016dc630a3
Deven Damiano <damiano.da96@gmail.com>
2019-04-11 11:29:03 -0400

    added some TODO's

--- preprocessor.py
+++ preprocessor.py
@@ -1,5 +1,5 @@
 import sys
-import pandas as pd # pandas is used to export csv
+import pandas as pd # pandas is used to create csv
 
 data_table = []
 i = 0
@@ -12,6 +12,17 @@ with open("ALL_vs_AML_train_set_38_sorted.res", "r") as file:
 data_frame = pd.DataFrame(data_table)
 data_frame.columns = data_table[0]
 data_frame = data_frame.drop(data_frame.index[[0,1,2]]) # drop all unnecessary rows
+
+# TODO:: REMOVE THE FOLLOWING FROM OUR DATA SET
+# - (endogenous) control genes
+# - genes with A's across the experiments
+# - genes with less than two fold change across the experiments (max/min <2);
+# TODO:: REPLACE EXPR VALUES BELIOW THRESHHOLD WITH THRESHHOLD VALUE
+# TODO:: REORGANIZE REMAINING DATA TO ONLY SHOW EXPR VALUES
+
+
+
+
 data_frame.to_csv('training_data.csv', index=False) # export csv of data
 
 # TODO: remove rows as stated by the project instructions
