debb8788c9ae10853a0c7aeae01e84ab096008fe
Deven Damiano <damiano.da96@gmail.com>
2019-04-11 10:02:42 -0400

    data now is exported to csv

--- preprocessor.py
+++ preprocessor.py
@@ -1,14 +1,19 @@
 import sys
-import numpy as np
+import pandas as pd # pandas is used to export csv
 
 data_table = []
 i = 0
 
 with open("ALL_vs_AML_train_set_38_sorted.res", "r") as file:
 	for line in file:
-		cells = line.split()
+		cells = line.split("\t")
 		data_table.append(cells)
 
-for i in data_table[0]:
-	print(i)
+data_frame = pd.DataFrame(data_table)
+data_frame.columns = data_table[0]
+data_frame = data_frame.drop(data_frame.index[[0,1,2]]) # drop all unnecessary rows
+data_frame.to_csv('training_data.csv', index=False) # export csv of data
+
+# TODO: remove rows as stated by the project instructions
+	
 		
