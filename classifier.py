8959af8f28e931aa3f79f6d82007809f523c7764
Deven Damiano <damiano.da96@gmail.com>
2019-04-13 13:04:59 -0400

    KNN Classifier file created

---
+++ classifier.py
@@ -0,0 +1,11 @@
+from sklearn.neighbors import KNeighborsClassifier
+
+# Some framework for the classifier
+
+model = KNeighborsClassifier(n_neighbors = 3)
+
+# Train the model on our training sets
+# model.fit(features, label
+
+# Predict Output
+#predicted = model.predict([[]])
